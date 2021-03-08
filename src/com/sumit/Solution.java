947be08c76e94bbd145443f260fa5bf16e98cabc
yaminikrishna07 <yaminikrishna3127@gmail.com>
2021-03-08 11:45:14 -0500

    Update Made

--- src/com/sumit/Solution.java
+++ src/com/sumit/Solution.java
@@ -1,13 +1,27 @@
 package com.sumit;
 
 public class Solution {
-
-	public static int removeDuplicates(int[] nums) {
+    
+	/* Constraint is:
+	 * We can only use O(1) space so we can't use any extra space.
+	 * 
+	 * Algorithm working:
+	 * 1. take two pointers.
+	 * 2. since array is already sorted according to problem. we check if current element is not equal to 
+	 * next element. if it is not, then we increase the pointer i and copy the value of nums[j] to
+	 * nums[i+1]. and then increase the j pointer and continue the process.
+	 * 
+	 * 3. to get length add 1 to last i we get after going through array as indexes are 0 based.
+	 */
+	
+	public static int removeDuplicates(int[] nums) 
+	{
         if(nums.length==0){
             return 0;
         }
         
         int i =0;
+        
         
         for(int j = 1 ; j<nums.length ; j++)
         {
